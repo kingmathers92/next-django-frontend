4ac2c3aa41df98bcbd9d50df7c91bf3b9dfdafc1
KHALED\E392 <khaledb.yahya@gmail.com>
2024-07-17 03:09:19 +0100

    auth token methods

--- src/app/api/login/route.jsx
+++ src/app/api/login/route.jsx
@@ -1,19 +1,40 @@
 "use server";
+import {
+  getRefreshToken,
+  getToken,
+  setRefreshToken,
+  setToken,
+} from "@/app/lib/auth";
 import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 
-export async function POST(request) {
-  const data = await request.json();
-  console.log(data);
+const DJANGO_API_LOGIN_URL = "http://127.0.0.1:8001/api/token/pair";
 
-  const authToken = cookies().get("auth-token");
-  cookies().set({
-    name: "auth-token",
-    value: "abc",
-    httpOnly: true, // limit client-side js
-    sameSite: "strict",
-    secure: process.env.NODE_ENV !== "development",
-    maxAge: 3600,
-  });
+export async function POST(request) {
+  const myAuthToken = getToken();
+  const myRefreshToken = getRefreshToken();
+  console.log(myAuthToken, myRefreshToken);
+
+  const requestData = await request.json();
+  const jsonData = JSON.stringify(requestData);
+  const requestOptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: jsonData,
+  };
+  const response = await fetch(DJANGO_API_LOGIN_URL, requestOptions);
+  const responseData = await response.json();
+  console.log(responseData);
+  if (response.ok) {
+    console.log("logged in");
+    const { access, refresh } = responseData;
+    setToken(access);
+    setRefreshToken(refresh);
+  }
+
+  //const authToken = cookies().get("auth-token");
+
   return NextResponse.json({ hello: "world" }, { status: 200 });
 }
