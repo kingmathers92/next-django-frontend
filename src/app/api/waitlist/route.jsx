9a547f06a95d9e7af43add76cc0aaa58f9249e56
KHALED\E392 <khaledb.yahya@gmail.com>
2024-07-22 23:03:57 +0100

    env variables config

--- src/app/api/waitlist/route.jsx
+++ src/app/api/waitlist/route.jsx
@@ -1,8 +1,9 @@
 import { getToken } from "@/lib/auth";
 import { NextResponse } from "next/server";
 import ApiProxy from "../proxy";
+import { DJANGO_API_ENDPOINT } from "@/config/defaults";
 
-const DJANGO_API_WAITLIST_URL = "http://127.0.0.1:8001/api/waitlist/";
+const DJANGO_API_WAITLIST_URL = `${DJANGO_API_ENDPOINT}/waitlist/`;
 
 export async function GET(request) {
   const { data, status } = await ApiProxy.get(DJANGO_API_WAITLIST_URL, true);
