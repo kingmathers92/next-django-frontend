2f2b5fee2c68298def75c116dde810522d342e9b
KHALED\E392 <khaledb.yahya@gmail.com>
2024-08-02 22:55:00 +0100

    added comments

--- src/app/api/proxy.jsx
+++ src/app/api/proxy.jsx
@@ -1,10 +1,11 @@
 import { getToken } from "@/lib/auth";
 
 export default class ApiProxy {
+  // Method to get headers for API requests, including the Authorization header if authentication is required
   static async getHeaders(requireAuth) {
     let headers = {
       "Content-Type": "application/json",
-      "Accept": "application/json",
+      Accept: "application/json",
     };
     const authToken = getToken();
     if (authToken && requireAuth === true) {
@@ -13,6 +14,7 @@ export default class ApiProxy {
     return headers;
   }
 
+  // Method to handle the fetch request and process the response
   static async handleFetch(endpoint, requestOptions) {
     let data = {};
     let status = 500;
@@ -27,6 +29,7 @@ export default class ApiProxy {
     return { data, status };
   }
 
+  // Method for sending a PUT request to the specified endpoint with the given object and optional authentication
   static async put(endpoint, object, requireAuth) {
     const jsonData = JSON.stringify(object);
     const headers = await ApiProxy.getHeaders(requireAuth);
@@ -38,6 +41,7 @@ export default class ApiProxy {
     return await ApiProxy.handleFetch(endpoint, requestOptions);
   }
 
+  // Method for sending a DELETE request to the specified endpoint with optional authentication
   static async delete(endpoint, requireAuth) {
     const headers = await ApiProxy.getHeaders(requireAuth);
     const requestOptions = {
@@ -47,6 +51,7 @@ export default class ApiProxy {
     return await ApiProxy.handleFetch(endpoint, requestOptions);
   }
 
+  // Method for sending a POST request to the specified endpoint with the given object and optional authentication
   static async post(endpoint, object, requireAuth) {
     const jsonData = JSON.stringify(object);
     const headers = await ApiProxy.getHeaders(requireAuth);
@@ -58,6 +63,7 @@ export default class ApiProxy {
     return await ApiProxy.handleFetch(endpoint, requestOptions);
   }
 
+  // Method for sending a GET request to the specified endpoint with optional authentication
   static async get(endpoint, requireAuth) {
     const headers = await ApiProxy.getHeaders(requireAuth);
     const requestOptions = {
